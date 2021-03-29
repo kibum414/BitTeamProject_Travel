cb91a0c2bb4baebf2c40fb083ef5ec00d2410486
kibum414 <kibum414@naver.com>
2021-03-30 00:38:45 +0900

    210330

--- fe/src/pts/component/ParticipantList.js
+++ fe/src/pts/component/ParticipantList.js
@@ -1,7 +1,56 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom'
+import axios from 'axios'
+import '../css/List.css'
+
 const ParticipantList = () => {
+
+  const [list, setList] = useState([])
+
+  const getList = () => {
+    axios.get(`http://localhost:8080/participants`)
+      .then(res => {
+        console.log(res)
+        setList(res.data)
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
+  useEffect(() => {
+    console.log('getList')
+
+    getList()
+  }, [])
 
   return (
     <>
+      <table>
+        <thead>
+          <tr>
+            <th>번호</th>
+            <th colSpan="10">이름</th>
+            <th>신청일</th>
+          </tr>
+        </thead>
+        <tbody>
+          {
+            list.map(participant =>
+              <tr>
+                <td key={participant.participantNo}>{participant.participantNo}</td>
+                
+                <td colSpan="10">
+                  <Link to={`/participants/list/${participant.participantNo}`}>
+                    {participant.name}
+                  </Link>
+                </td>
+                  <td>{participant.regdate}</td>
+              </tr>
+            )
+          }
+        </tbody>
+      </table>
     </>
   )
 }
